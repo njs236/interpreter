e81749943fdf911bece08b42e258151045c0c3d2
Nathan Sinclair <njs236@student.cpit.ac.nz>
2016-03-13 17:32:40 +1300

    implement of console and exception handling

--- interpreter.py
+++ interpreter.py
@@ -4,6 +4,7 @@ import io
 import abc
 import csv
 import sys
+import cmd
 
 
 
@@ -13,12 +14,53 @@ class FileReader(metaclass=abc.ABCMeta):
     def openFile(self):
         pass
 
+class MyError(Exception):
+    def __init__(self, line):
+        self.line = line
+
+    def __str__(self):
+        return self.line
+
 class DataChecker(metaclass=abc.ABCMeta):
     def makeCheckers(self):
         print("")
     def check(self):
         print("")
 
+class Console(cmd.Cmd):
+    def __init__(self):
+        """
+
+        :return: None
+        """
+        cmd.Cmd.__init__(self)
+        self.prompt = "=>> "
+        self.intro  = "Welcome to console!"  ## defaults to None
+    def do_exit(self, line):
+        """
+        to Exit from the console
+        :param line: a message
+        :return: to Exit from console
+        """
+        return -1
+
+    def do_q(self,line):
+        """
+        To exit from the console
+        :param line: a message
+        :return: to call Exit method
+        """
+        return self.do_exit(line)
+    def do_help(self, args):
+        """
+        Get help on commands
+           'help' or '?' with no arguments prints a list of commands for which help is available
+           'help <command>' or '? <command>' gives help on <command>
+        :param args: a message
+        :return: None
+        """
+        ## The only reason to define this method is for the help text in the doc string
+        cmd.Cmd.do_help(self, args)
 
 class Controller(FileReader, DataChecker):
 # instancing the model and views in this way is a design decision, the views should be initialised when the program is started
@@ -29,6 +71,8 @@ class Controller(FileReader, DataChecker):
         self.DataColumns = {"0": "ID","1": "Gender","2": "Age", "3": "Sales", "4" :"BMI","5" :"Income"}
         self.myModel = myModel
         self.myViews = myViews
+        self.bmireader = ""
+        self.csvfile = ""
 
     """
     def controllerLoop(self):
@@ -39,7 +83,7 @@ class Controller(FileReader, DataChecker):
         #testcase, if data is loaded it will print the data in lines. Success
         filename = sys.argv[1]
         csvfile = open(filename, newline='')
-        self.bmireader = csv.reader(csvfile, delimiter = ' ', quotechar= '|')
+        bmireader = csv.reader(self.csvfile, delimiter = ' ', quotechar= '|')
         for row in self.bmireader:
             print(', '.join(row))
         #testcase, see if multiple rows load. Success
@@ -84,26 +128,32 @@ class Controller(FileReader, DataChecker):
         #uses input checkers to determine the right data set
         #
         wrongLines = 0
+
         filename = sys.argv[1]
         csvfile = open(filename, newline='')
         bmireader = csv.reader(csvfile, delimiter = ' ', quotechar= '|')
-        check = bmireader != None
-        for row in bmireader:
-            wrong = self.readLine(row)
-            if(wrong==False):
-                self.myModel.addData(ID=row[0], Gender=row[1], Age=row[2], Sales=row[3], BMI=row[4], Income=row[5])
-            elif (wrong==True):
-                wrongLines += 1
-        #case of wronglines of code
-        if (wrongLines > 0):
-            print("We found " + str(wrongLines) + " row of wrong data")
-        csvfile.close()
-        self.myModel.countData()
+
+        try:
+            for row in bmireader:
+                wrong = self.readLine(row)
+                if(wrong==False):
+                    self.myModel.addData(ID=row[0], Gender=row[1], Age=row[2], Sales=row[3], BMI=row[4], Income=row[5])
+                elif (wrong==True):
+                    wrongLines += 1
+            #case of wronglines of code
+            if (wrongLines > 0):
+                print("We found " + str(wrongLines) + " row of wrong data")
+            csvfile.close()
+        except MyError:
+            print("Incorrect Input Length")
+            """
         for item in self.myModel.allMyData:
             print(item)
+            """
 
     def readLine(self,line):
-        print(line)
+        if (line[5] == None):
+            raise(MyError("here is my error"))
         headings = [self.DataColumns['0'],
                     self.DataColumns['1'],
                     self.DataColumns['2'],
@@ -114,12 +164,9 @@ class Controller(FileReader, DataChecker):
         for item in range(0,5):
             if self.myModel.findInputChecker(headings[item]):
                 IC = self.myModel.findInputChecker(headings[item])
-                print("line: " + line[item])
                 if (IC.isValid(line[item])):
-                    print("passed")
                     pass
                 else:
-                    print("failed")
                     return True
         return False
 
@@ -230,3 +277,5 @@ if (__name__ == '__main__'):
     controller.openFile()
     controller.makeCheckers()
     controller.check()
+    console = Console()
+    console.cmdloop()
