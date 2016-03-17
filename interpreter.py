b4b6476c85cf02262b956b28f052b9d3d7423118
njs236 <sinclair.nathan@gmail.com>
2016-03-18 12:51:09 +1300

    testing

--- interpreter.py
+++ interpreter.py
@@ -13,19 +13,47 @@ import unittest
 class MainTest(unittest.TestCase):
 
     def setUp(self):
-        pass
+        model = Model()
+        views = [ConsoleView("Console"), PieView("Pie"), ChartView("Chart")]
+        self.myController = Controller(model, views)
+        self.myController.makeCheckers()
 
     def tearDown(self):
         pass
 
     def test01(self):
+        #There should be 6 checkers
+        actual = self.myController.myModel.countIC()
+        expected = 6
+
+        #the assert
+        self.assertEqual(actual,expected, "There are 6 Input Checkers")
         pass
 
     def test02(self):
-        pass
+        #Check to find an input checker and its constraint
+        firstchecker = self.myController.myModel.findInputChecker('ID')
+        expectedconstraint ="[A-Z][0-9]{3}"
+
+        #assert
+        self.assertTrue(firstchecker.getConstraint() == expectedconstraint, "The id checker should have constraint [A-Z][0-9]{3}")
+
 
     def test03(self):
-        pass
+        #check filename Input and console input
+        filename = 'E:\PycharmProjects\interpreter\data.csv'
+        actual = self.myController.check(filename)
+        expected = self.myController.myModel.findData('A001')
+
+        #assert
+
+        self.assertIsNotNone(expected, "handles filename input")
+
+    def test04(self):
+        #give some invalid input expected a message
+        filename = "E:\PycharmProjects\interpreter\data2.csv"
+        actual = self.myController.check(filename)
+
 
 class FileReader(metaclass=abc.ABCMeta):
     def openFile(self, filename):
@@ -45,12 +73,13 @@ class DataChecker(metaclass=abc.ABCMeta):
         print("")
 
 class Console(cmd.Cmd):
-    def __init__(self):
+    def __init__(self, view):
         """
 
         :return: None
         """
         cmd.Cmd.__init__(self)
+        self.myView = view
         self.prompt = "=>> "
         self.intro  = "Welcome to console!"  ## defaults to None
     def do_exit(self, line):
@@ -78,6 +107,23 @@ class Console(cmd.Cmd):
         """
         ## The only reason to define this method is for the help text in the doc string
         cmd.Cmd.do_help(self, args)
+    def do_age(self, args):
+        self.myView.getController().displayAgeGraph()
+
+    def do_bmi(self, args):
+        self.myView.getController().displayBMIGraph()
+
+    def do_gender(self, args):
+        self.myView.getController().displayGenderGraph()
+
+    def do_sales(self, args):
+        self.myView.getController().displaySalesGraph()
+
+    def do_income(self, args):
+        self.myView.getController().displayIncomeGraph()
+
+    def do_bmisales(self, args):
+        self.myView.getController().displayBMISalesGraph()
 
 class Controller(FileReader, DataChecker):
 # instancing the model and views in this way is a design decision, the views should be initialised when the program is started
@@ -90,16 +136,68 @@ class Controller(FileReader, DataChecker):
         self.myViews = myViews
         self.bmireader = ""
         self.csvfile = ""
+        self.linkViewsToController()
 
     """
     def controllerLoop(self):
 
 
     """
+    def displayAgeGraph(self):
+        """
+
+         Age Graph is a bar chart that displays age by range
+         0-10,
+         11-20,
+         21-30,
+         31-40,
+         41-50,
+         50+
+
+        :return:
+        """
+        age = self.myModel.getAgeData()
+        data = {'x': ['0-10', '11-20', '21-30', '31-40', '41-50', '50+'],
+                'y': age,
+                'title': 'BMI Age by bracket'}
+
+        plotly = self.findViewById('Chart');
+        plotly.printLine("age")
+        plotly.display(data)
+    def displayBMIGraph(self):
+        plotly = self.findViewById('Plotly');
+        plotly.printLine("stub")
+    def displayGenderGraph(self):
+        plotly = self.findViewById('Plotly');
+        plotly.printLine("stub")
+    def displaySalesGraph(self):
+        plotly = self.findViewById('Plotly');
+        plotly.printLine("stub")
+    def displayIncomeGraph(self):
+        plotly = self.findViewById('Plotly');
+        plotly.printLine("stub")
+    def displayBMISalesGraph(self):
+        plotly = self.findViewById('Plotly');
+        plotly.printLine("stub")
+
+    def linkViewsToController(self):
+        for item in self.myViews:
+            item.setController(self)
+
+    def findViewById(self, id):
+        for item in self.myViews:
+            if item.getID() == id:
+                return item
+        return None
+
     def openFile(self, filename):
+        try:
+
         #testcase, if data is loaded it will print the data in lines. Success
-        csvfile = open(filename, newline='')
-        return csv.reader(self.csvfile, delimiter = ' ', quotechar= '|')
+            csvfile = open(filename, newline='')
+            return csv.reader(csvfile, delimiter = ' ', quotechar= '|')
+        except OSError:
+            print("Invalid Filename")
         #testcase, see if multiple rows load. Success
 
 
@@ -138,21 +236,15 @@ class Controller(FileReader, DataChecker):
         self.myModel.shelveObjects()
 
 
-    def check(self):
-        """
-        >>> print(self.myModel.findData('A001'))
-        A001, Male, 36, 455, Normal, 889
-        :param id:
-        :return:
-        """
+    def check(self, filename):
         #needs bmireader data.
         #sets up a count of how many wrong lines there are
         #uses input checkers to determine the right data set
         #
         wrongLines = 0
-        filename = sys.argv[1]
         if filename == None:
-            filename = self.myViews[0].takeInput("Enter a valid filename:")
+            filename = self.myViews[0].readLine("Enter a valid filename:")
+
         bmireader = self.openFile(filename)
 
         try:
@@ -166,17 +258,12 @@ class Controller(FileReader, DataChecker):
             #case of wronglines of code
             if (wrongLines > 0):
                 print("We found " + str(wrongLines) + " row of wrong data")
-        except MyError:
+        except IndexError:
             print("Incorrect Input Length")
-            """
-        for item in self.myModel.allMyData:
-            print(item)
-            """
+
 
 
     def readLine(self,line):
-        if (line[5] == None):
-            raise(MyError("here is my error"))
         headings = [self.DataColumns['0'],
                     self.DataColumns['1'],
                     self.DataColumns['2'],
@@ -202,16 +289,76 @@ class Controller(FileReader, DataChecker):
 
 
 class IView(metaclass=abc.ABCMeta):
+    def getID(self):
+        pass
+    def readLine(self, string):
+        pass
+    def setController(self, controller):
+        pass
+    def getController(self):
+        pass
 
-    def ReadLine(self, string):
-        return string
-    def display(self):
-        return
+class PieView(IView):
+    def __init__(self, id):
+        self.id = id
+    def getID(self):
+        return self.id
+    def readLine(self, string):
+        return input(string)
+    def setController(self, controller):
+        self.myController = controller
+    def getController(self):
+        return self.myController
+    def printLine(self, line):
+        print(line)
 
-class View():
-    def display(self):
-        return
+    def display(self, args):
+        import plotly
 
+        fig = {'data': [{'labels': args['labels'],
+                         'values': args['values'],
+                         'type': args['pie']}],
+               'layout': {'title': args['title']}
+               }
+
+        plotly.offline.plot(fig)
+
+class ChartView(IView):
+    def __init__(self, id):
+        self.id = id
+    def getID(self):
+        return self.id
+    def readLine(self, string):
+        return input(string)
+    def setController(self, controller):
+        self.myController = controller
+    def getController(self):
+        return self.myController
+    def printLine(self, line):
+        print(line)
+
+    def display(self, args):
+        import plotly as py
+        import plotly.graph_objs as go
+
+        data = [go.Bar(y= args['y'],
+                         x= args['x'])]
+        layout = go.Layout(title= args['title'])
+        fig = go.Figure(data = data, layout=layout)
+        py.offline.plot(fig)
+
+
+class ConsoleView(IView):
+    def __init__(self, id):
+        self.id = id
+    def getID(self):
+        return self.id
+    def readLine(self, string):
+        return input(string)
+    def setController(self, controller):
+        self.myController = controller
+    def getController(self):
+        return self.myController
 
 
 class Model(object):
@@ -228,6 +375,30 @@ class Model(object):
         for item in self.allMyData:
             db[item.getID()] = item
         db.close()
+
+    def getAgeData(self):
+        """
+        I need all data on age
+        :return: age in brackets
+        """
+        list= [0, 0, 0,  0, 0,  0]
+        for item in self.allMyData:
+            if item.getAge() <= 10:
+                list[0] += 1
+            elif item.getAge() <=20:
+                list[1] += 1
+            elif item.getAge() <= 30:
+                list[2] += 1
+            elif item.getAge() <=40:
+                list[3] += 1
+            elif item.getAge() <=50:
+                list[4] += 1
+            else:
+                list[5] += 1
+
+        return list
+
+
 
     def addRegExp(self, id, constraint, description):
         newIC = RegExp(id, constraint, description)
@@ -250,10 +421,10 @@ class Model(object):
         return None
 
     def countIC(self):
-        print(len(self.allMyInputCheckers))
+        return len(self.allMyInputCheckers)
 
     def countData(self):
-        print(len(self.allMyData))
+        return len(self.allMyData)
 
     def getAllMyCheckers(self):
         return self.allMyInputCheckers
@@ -277,6 +448,16 @@ class Data(object):
             pickle.dump(self, f)
     def getID(self):
         return self.id
+    def getAge(self):
+        return int(self.age)
+    def getSales(self):
+        return self.sales
+    def getBMI(self):
+        return self.BMI
+    def getGender(self):
+        return self.gender
+    def getIncome(self):
+        return self.income
 
     def __str__(self):
         return self.id + ", " + self.gender + ", " +  self.age + ", " + self.sales + ", " + self.BMI + ", " + self.income
@@ -290,6 +471,8 @@ class InputChecker(object):
 
     def isValid(self, data):
         return True
+    def getConstraint(self):
+        return self.constraint
 
     def __str__(self):
         return str(self.id) + " is a " + self.description + " with a " + self.constraint + " constraint"
@@ -316,14 +499,16 @@ class Enum(InputChecker):
             return False
 
 if (__name__ == '__main__'):
-    import doctest
     model = Model()
-    views = View()
+    views = [ConsoleView("Console"), PieView("Pie"), ChartView("Chart")]
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        filename = None
     controller = Controller(model, views)
-    controller.openFile()
     controller.makeCheckers()
-    controller.check()
-    doctest.testmod()
+    controller.check(filename)
+    #unittest.main(verbosity=2)
     controller.shelveObjects()
-    console = Console()
+    console = Console(views[0])
     console.cmdloop()
